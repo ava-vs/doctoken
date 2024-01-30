bd91bdb4673449e6f7b068ad791d712f581644e6
Ilia <info@ava.capetown>
2024-01-30 17:04:52 +0300

    add ic deploy script

---
+++ commands/deploy-ic.sh
@@ -0,0 +1,18 @@
+dfx canister create --all --ic && dfx build && dfx deploy --argument "(
+  record {
+    owner = principal\"$(dfx identity get-principal)\";
+    subaccount = opt blob \"00000000000000000000000000000000\";
+  },
+  record {
+    name = \"Your Doctoken\";
+    symbol = \"YOURDCT\";
+    royalties = opt 0;
+    royaltyRecipient = opt record {
+      owner = principal\"2vxsx-fae\";
+      subaccount = null;
+    };
+    description = opt \"Sample doctoken\";
+    image = null;
+    supplyCap = opt 10000;
+  }
+)" doctoken
